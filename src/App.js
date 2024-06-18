bced0333cf57a740e26e3ed9f8576b34d65b4d6a
spidy2301 <shresthgadhwal@gmail.com>
2024-06-18 20:43:34 +0530

    Path Setting to Different Pages

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
 
 import './App.css';
 import Navbar from './Components/Navbar/Navbar';
-
+import { BrowserRouter,Routes,Route } from 'react-router-dom';
+import Shop from './Pages/Shop';
+import ShopCategory from './Pages/ShopCategory';
+import Product from './Pages/Product';
+import Cart from './Pages/Cart';
+import LoginSignup from './Pages/LoginSignup';
 function App() {
   return (
-    <div >
+    <div>
+     <BrowserRouter>
       <Navbar/>
+      <Routes>
+        <Route path='/' element={<Shop/>}/>
+        <Route path='/' element={<ShopCategory category="mens"/>}/>
+        <Route path='/' element={<ShopCategory category="womens"/>}/>
+        <Route path='/' element={<ShopCategory category="kids"/>}/>
+        <Route path="/product" element={<Product/>}>
+          <Route path=':productId' element={<Product/>}/>
+        </Route>
+        <Route path='/' element={<Cart/>}/>
+        <Route path='/' element={<LoginSignup/>}/>
+      </Routes>
+      </BrowserRouter>
     </div>
   );
 }
